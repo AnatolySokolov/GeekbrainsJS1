06936934329c8ede47b0286d2374fa81966b3ca8
Anatoly <AnatoliySokolov1981@gmail.com>
2019-02-21 20:33:00 +0300

    task 1 complete

--- lesson 3/task1.js
+++ lesson 3/task1.js
@@ -3,6 +3,16 @@
 (function () {
   //1. С помощью цикла while вывести все простые числа в промежутке от 0 до 100.
 
+  let i = 0;
+
+  while (i < 100) {
+    i++;
+    if((i === 1 || i % 2 === 0) && i !== 2) continue;
+    if(i % 3 === 0 && i !== 3) continue;
+    if(i % 5 === 0 && i !==5) continue;
+    if(i % 7 === 0 && i !==7) continue;
+    console.log(i);
+  }
 
 })();
 
