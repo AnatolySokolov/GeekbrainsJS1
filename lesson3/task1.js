226496c3196dcdd4e4928b3812f1a85643fe6e86
Anatoly <AnatoliySokolov1981@gmail.com>
2019-02-21 23:33:50 +0300

    fixed task1

--- lesson3/task1.js
+++ lesson3/task1.js
@@ -5,16 +5,20 @@
 
   console.log('Задание 1');
 
-  let i = 0;
+  let i = 0,
+      arrOfPrimeNumbers = [];
 
   while (i < 100) {
     i++;
-    if((i === 1 || i % 2 === 0) && i !== 2) continue;
-    if(i % 3 === 0 && i !== 3) continue;
-    if(i % 5 === 0 && i !==5) continue;
-    if(i % 7 === 0 && i !==7) continue;
-    console.log(i);
+    if ((i === 1 || i % 2 === 0) && i !== 2) continue;
+    if (i % 3 === 0 && i !== 3) continue;
+    if (i % 5 === 0 && i !== 5) continue;
+    if (i % 7 === 0 && i !== 7) continue;
+    // console.log(i);
+    arrOfPrimeNumbers.push(i);
   }
+
+  console.log('Простые числа от 0 до 100 : ' + arrOfPrimeNumbers);
 
 })();
 
