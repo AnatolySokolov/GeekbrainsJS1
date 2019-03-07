7cc59d451d9cecf6ec6f2a04dc40e78e6cf9b980
Anatoly <AnatoliySokolov1981@gmail.com>
2019-03-07 05:54:19 +0300

    add: total price and quantity

--- lesson6/cart.js
+++ lesson6/cart.js
@@ -8,8 +8,10 @@
     add: function (id) {
       let item = findItemById(id, this.data);
       if (checkCart(id, this.list)) {
+        item.quantity ++;
         return false;
       }
+      item.quantity = 1;
       this.list.push(item);
     },
 
@@ -23,7 +25,7 @@
     },
 
     render: function () {
-      renderCart(this.list);
+      renderCart(this.list, this.checkTotal());
     },
 
     clearCartContainer: function () {
@@ -31,6 +33,14 @@
       while ($container.firstChild) {
         $container.removeChild($container.firstChild);
       }
+    },
+
+    checkTotal: function () {
+      let total = 0;
+      this.list.forEach(function (el) {
+        total += el.price * el.quantity;
+      });
+      return total;
     }
   };
 
@@ -57,16 +67,30 @@
     $item.querySelector('.cart-image').alt = o.name;
     $item.querySelector('.cart-name').textContent = o.name;
     $item.querySelector('.cart-price').textContent = '$' + o.price;
+    $item.querySelector('.cart-quantity span').textContent = o.quantity;
     $item.querySelector('.cart-btn').setAttribute('data-id', o.id);
     return $item;
   };
 
-  let renderCart = function (arr) {
-    let fragment = document.createDocumentFragment();
+  let renderCart = function (arr, total) {
     let $container = document.querySelector('.cart');
-    arr.forEach(function (el) {
-      fragment.appendChild(createCartItem(el));
-    });
-    $container.appendChild(fragment);
+    let $total = document.createElement('p');
+
+    $total.classList.add('.total');
+    $total.textContent = 'В корзине нет товаров';
+
+    if (arr.length !== 0) {
+      let fragment = document.createDocumentFragment();
+      arr.forEach(function (el) {
+        fragment.appendChild(createCartItem(el));
+      });
+      $container.appendChild(fragment);
+
+      $total.textContent = 'Общая стоимость товаров: $' + total;
+    }
+
+    $container.appendChild($total);
   };
+
+  cart.render();
 })();
